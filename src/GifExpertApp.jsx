75c17fc224adad636d92edba321c20425a7b012b
fernanda nuñez <fernanda.nunez55@gmail.com>
2024-07-12 19:07:20 -0400

    add styles css

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react'
+
 export const GifExpertApp = () => {
+  const [categories, setCategorias] = useState(['One Punch'])
+
+  const ApiKey = 'eA6bCQRyATkZhc0ffpQTJsBjYvURJQfL'
+
   return (
     <>
       <h1>GifExpertApp</h1>
+      <ol>
+        {
+        categories.map(category => {
+          return <li key={category}>{category}</li>
+        })
+}
+      </ol>
     </>
   )
 }
